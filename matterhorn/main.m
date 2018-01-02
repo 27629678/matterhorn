37a8d7922dc0e0863717fd35c41013aa8c817ae3
yuxiaohua <hzyuxiaohua@corp.netease.com>
2018-01-02 10:55:05 +0800

    Initial Commit

---
+++ matterhorn/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  matterhorn
+//
+//  Created by hzyuxiaohua on 02/01/2018.
+//  Copyright © 2018 hzyuxiaohua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
